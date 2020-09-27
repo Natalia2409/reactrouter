50842d3a9ba0046231e35b41d5564b87557f9cf1
Natalia <nataliarudomyr@gmail.com>
2020-09-27 23:28:17 +0300

    Updates

---
+++ precache-manifest.7ce86ee24929a005ccde3d180f79bb4a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ec08ed7d44c58271e6da551d8a8c74",
+    "url": "/reactrouter/index.html"
+  },
+  {
+    "revision": "12c7cee6a9bbc3e6d51a",
+    "url": "/reactrouter/static/css/main.24cb1bae.chunk.css"
+  },
+  {
+    "revision": "e25483189fbc9bbcc76d",
+    "url": "/reactrouter/static/js/2.d0448a7f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactrouter/static/js/2.d0448a7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c7cee6a9bbc3e6d51a",
+    "url": "/reactrouter/static/js/main.8c75fa15.chunk.js"
+  },
+  {
+    "revision": "c77078ac330a06f32b23",
+    "url": "/reactrouter/static/js/runtime-main.c98c4bf1.js"
+  },
+  {
+    "revision": "a35cca28280e7e1b5fbd0d17258061af",
+    "url": "/reactrouter/static/media/1.a35cca28.jpg"
+  },
+  {
+    "revision": "0add0b8379f9c078ab92c3d0e9265e09",
+    "url": "/reactrouter/static/media/2.0add0b83.jpg"
+  },
+  {
+    "revision": "4c6d4458e7b4ef712c3025f7f5d13c2f",
+    "url": "/reactrouter/static/media/3.4c6d4458.jpg"
+  },
+  {
+    "revision": "321b22feaf715adfea76bd59240b74c5",
+    "url": "/reactrouter/static/media/female-icon.321b22fe.jpg"
+  },
+  {
+    "revision": "592396bc148a5dbf49971ff36ffbf1bf",
+    "url": "/reactrouter/static/media/male-icon.592396bc.jpg"
+  },
+  {
+    "revision": "ab7f36c566c2b80cb351ae3a550527a8",
+    "url": "/reactrouter/static/media/unknown.ab7f36c5.png"
+  }
+]);
